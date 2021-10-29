4854e5cba620d5f843ac1dabe990fc333af84da9
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-10-29 07:34:09 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const Equipe = (props) => {
+  return (
+    <div>
+      <Sobre username={props.nome} cargo={props.cargo} idade={props.idade} />
+      <Social fb={props.facebook} />
+      <hr></hr>
+    </div>
+  )
+}
+
+const Sobre = (props) => {
+  return (
+    <div>
+      <h2>Olá sou o(a) {props.username}</h2>
+      <h3>Cargo: {props.cargo}</h3>
+      <h3>Idade: {props.idade} anos</h3>
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return (
+    <div>
+      <a href={props.fb}>Facebook</a>
+      <a href>LinkedIn</a>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe nome="Romulo" cargo="Programador" idade="21" facebook="https://google.com.br" />
+      <Equipe nome="Rafanone" cargo="Dev Back-end" idade="20" facebook="https://facebook.com" />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
